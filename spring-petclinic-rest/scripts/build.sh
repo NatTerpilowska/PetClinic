dc7b2c2758cb902fdbea95d9ad896f131cf451a3
Ubuntu <michaelroy@MR-Workspace.xfv2txqrjp2urjgrj3ylnyfysd.zx.internal.cloudapp.net>
2021-09-07 08:44:02 +0000

    MR nginx.conf and updated build.sh

--- spring-petclinic-rest/scripts/build.sh
+++ spring-petclinic-rest/scripts/build.sh
@@ -1,1 +1,3 @@
-docker-compose up -d --build 
+#docker-compose up -d --build 
+docker run -d -p 80:8080 --name petclinicfront spring-petclinic-angular:latest 
+docker run -d -p 9966:9966 -n back springcommunity/spring-petclinic-rest
