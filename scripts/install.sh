f105af83134e475029bc11f19e6c683c9a130710
Ubuntu <michaelroy@MR-Workspace.xfv2txqrjp2urjgrj3ylnyfysd.zx.internal.cloudapp.net>
2021-09-08 14:01:00 +0000

    MR updated instastall sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -29,7 +29,9 @@ sudo chmod +x /usr/local/bin/docker-compose
 # terraform --version 
 cd spring-petclinic-angular
 sudo apt install npm -y
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+nvm install 12.14.1
 sudo npm uninstall -g angular-cli @angular/cli
 npm cache clean
 sudo npm install -g @angular/cli@latest
