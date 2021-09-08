dcddfea02a5c9349cc6f4d80f5d026ec9d060834
Ubuntu <michaelroy@MR-Workspace.xfv2txqrjp2urjgrj3ylnyfysd.zx.internal.cloudapp.net>
2021-09-08 14:12:21 +0000

    MR updated instastall sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 #! bin/bash
 cd spring-petclinic-angular
+nvm install 12.14.1
 alias launchchrome="\"/mnt/c/Program Fles (x86)/Google/Chrome/Application/chrome.exe\""
 export CHROME_BIN=launchchrome
 ng test
