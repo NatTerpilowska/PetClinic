bad01ccde21da85eb211992621b499e4e084f31a
Ubuntu <michaelroy@MR-Workspace.xfv2txqrjp2urjgrj3ylnyfysd.zx.internal.cloudapp.net>
2021-09-08 11:43:14 +0000

    jenkins scrips update

--- spring-petclinic-rest/scripts/test.sh
+++ spring-petclinic-rest/scripts/test.sh
@@ -1,10 +1,8 @@
 #! bin/bash
-cd ..
 cd spring-petclinic-angular
 alias launchchrome="\"/mnt/c/Program Fles (x86)/Google/Chrome/Application/chrome.exe\""
 export CHROME_BIN=launchchrome
 ng test
-cd ..
 
-# Unsure of what the java command is for testing?
+
 
