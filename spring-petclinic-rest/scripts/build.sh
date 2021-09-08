ab4e90b805acedced7cc52b0990a119e1892e954
Ubuntu <michaelroy@MR-Workspace.xfv2txqrjp2urjgrj3ylnyfysd.zx.internal.cloudapp.net>
2021-09-08 11:59:31 +0000

    jenkins scrips update

--- spring-petclinic-rest/scripts/build.sh
+++ spring-petclinic-rest/scripts/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
 set -e
-
-
-docker-compose build --parallel && \
-
+docker-compose --build --parallel && \
 docker login -u ${DOCKERHUB_CREDENTIALS_USR} -p ${DOCKERHUB_CREDENTIALS_PSW}
 docker-compose push
