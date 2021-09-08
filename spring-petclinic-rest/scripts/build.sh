4bdcdfbe3913a447959be30a93fc060fc8142bc1
Ubuntu <michaelroy@MR-Workspace.xfv2txqrjp2urjgrj3ylnyfysd.zx.internal.cloudapp.net>
2021-09-08 09:16:00 +0000

    MR finished docker files with DB

--- spring-petclinic-rest/scripts/build.sh
+++ spring-petclinic-rest/scripts/build.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-# docker run -d -p 80:8080 --name petclinicfront spring-petclinic-angular:latest 
-# docker run -d -p 9966:9966 --name petclinicback springcommunity/spring-petclinic-rest
 
 docker-compose build --parallel && \
 
-#docker login -u ${DOCKERHUB_CREDENTIALS_USR} -p ${DOCKERHUB_CREDENTIALS_PSW}
+docker login -u ${DOCKERHUB_CREDENTIALS_USR} -p ${DOCKERHUB_CREDENTIALS_PSW}
 docker-compose push
