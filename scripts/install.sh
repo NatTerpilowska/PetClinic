9a87dd39e726b839eb75193918410cca7034347d
Ubuntu <michaelroy@MR-Workspace.xfv2txqrjp2urjgrj3ylnyfysd.zx.internal.cloudapp.net>
2021-09-08 15:25:18 +0000

    MR updated instastall sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -39,7 +39,8 @@ npm install --save-dev @angular/cli@latest
 
 npm install
 npm i --save-dev karma karma-chrome-launcher
+npm update
 sudo apt-get install chromium-browser -y
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 cd ..
 
