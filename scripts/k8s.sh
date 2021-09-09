b376a5a4c56d131e4706f7848876718f3fb7666f
Ubuntu <michaelroy@MR-Workspace.xfv2txqrjp2urjgrj3ylnyfysd.zx.internal.cloudapp.net>
2021-09-09 14:43:23 +0000

    MR Terra jenkins

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az login --identity 
-az aks get-credentials --resource-group Project --name petclinic-cluster
+az aks get-credentials --resource-group project-rG --name project-aks
 # Front-end
 kubectl apply -f spring-petclinc-angular.yaml
 # Backend 
