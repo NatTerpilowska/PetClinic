bc894d6ee23f742434f238d90bec1bcbdb4e7be2
Nat <natterpilowska@gmail.com>
2021-09-09 10:52:16 +0000

    Fixed Kubernetes and Terraform NT and CB

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,9 @@
-output "group_name" {
-    value = azurerm_resource_group.main.name
+output "client_certificate" {
+  value = module.k8s-cluster.client_certificate
+}
+
+output "kube_config" {
+  value = module.k8s-cluster.kube_config
+
+  sensitive = true
 }
