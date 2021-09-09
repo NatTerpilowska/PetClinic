6cd2dc4e67f0e0bef673cd02dba017b01f6a9ebe
Ubuntu <michaelroy@MR-Workspace.xfv2txqrjp2urjgrj3ylnyfysd.zx.internal.cloudapp.net>
2021-09-09 09:26:14 +0000

    MR wOrKinG tEsTs and working build build script

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,22 +18,4 @@ sudo curl -L "https://github.com/docker/compose/releases/download/${version}/doc
 # make the file executable
 sudo chmod +x /usr/local/bin/docker-compose
 
-# cd spring-petclinic-angular
-# rm -rf node_modules
-# rm -f *.deb
-# sudo apt install npm -y
-
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-# sudo npm uninstall -g angular-cli @angular/cli
-# sudo npm cache clean --force
-# sudo npm install -g @angular/cli@8
-# npm install --save-dev @angular/cli@8
-
-# npm install
-# npm i --save-dev karma karma-chrome-launcher
-
-# sudo apt-get install chromium-browser -y
-
-# cd ..
 
