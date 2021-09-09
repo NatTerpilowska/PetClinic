5a0e5434ba8273895d2e253b3b53586bb5067f02
Ubuntu <michaelroy@MR-Workspace.xfv2txqrjp2urjgrj3ylnyfysd.zx.internal.cloudapp.net>
2021-09-09 14:50:54 +0000

    MR Terra jenkins

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -2,7 +2,5 @@
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az login --identity 
 az aks get-credentials --resource-group project-rG --name project-aks
-# Front-end
-kubectl apply -f spring-petclinc-angular.yaml
-# Backend 
-kubectl apply -f spring-petclinc-rest.yaml
+
+kubectl apply -f k8s
